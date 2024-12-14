4f64965aa3d2529c4a1065f64d72e5e60c24f147
q-1-p <github.pubilc@outlook.com>
2024-12-14 15:20:44 +0900

    fix

--- src/tests/content.test.jsx
+++ src/tests/content.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Content from '../components/content';
+import Content from '../components/Content';
 import { findAll, deleteRecord } from '../infrastructure/repo';
 
 // Mock the repository functions
